b2e5728973b6a6ca0be41e16119bd63dd2b85a55
MarcusViniLima <marcus12367lima@gmail.com>
2023-01-26 23:57:38 -0300

    Swagger

--- src/main/java/com/api/testetecnico/TesteTecnicoApplication.java
+++ src/main/java/com/api/testetecnico/TesteTecnicoApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
 @SpringBootApplication
+
 public class TesteTecnicoApplication {
 
 	public static void main(String[] args) {
